230a60d3bd4d5862fa142a2bf67d507e878bd77a
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-07-12 14:11:30 +0900

    Initial Commit

---
+++ PretendBusy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PretendBusy
+//
+//  Created by Naoto Ohno on 2017/07/12.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
