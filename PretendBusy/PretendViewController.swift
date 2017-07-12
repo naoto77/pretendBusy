14c17b31f2356d20658f3686b89e2a3b9b814ecb
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-07-12 16:06:11 +0900

    refs #NoTicket 必要なviewControllerファイルを作成

---
+++ PretendBusy/PretendViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PretendViewController.swift
+//  PretendBusy
+//
+//  Created by Naoto Ohno on 2017/07/12.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class PretendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
